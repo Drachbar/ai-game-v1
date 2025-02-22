e93ee357559e60d4b8650e37075bc38a34cd7574
Mattias <drachbar@gmail.com>
2025-02-22 13:52:05 +0100

    refaktorisea lite

--- backend/src/main/java/se/drachbar/aigamev1/service/ChatService.java
+++ backend/src/main/java/se/drachbar/aigamev1/service/ChatService.java
@@ -1,39 +1,36 @@
 package se.drachbar.aigamev1.service;
 
-
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.model.chat.request.ChatRequest;
-import dev.langchain4j.model.openai.OpenAiChatModelName;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import se.drachbar.aigamev1.chat.MyStreamingResponseHandler;
-import se.drachbar.aigamev1.config.ApiKeyConfig;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
 
-    private final StreamingChatLanguageModel model;
-
-    public ChatService(ApiKeyConfig apiKeyConfig) {
-        String apiKey = apiKeyConfig.getApiKey();
-        this.model = OpenAiStreamingChatModel.builder()
-                .apiKey(apiKey)
-                .modelName(OpenAiChatModelName.GPT_4_O_MINI)
-                .build();
-    }
+    private final Map<String, OpenAiStreamingChatModel> models;
 
     public void processQuery(String query, WebSocketSession session) {
-        List<ChatMessage> messages = new ArrayList<>();
-        messages.add(new SystemMessage("You are a helpful AI agent, you answer short and concise."));
-        messages.add(new SystemMessage("You are an expert in Java and Angular."));
-        messages.add(new UserMessage(query));
+        processQuery(query, session, "gpt4oMiniStreamingModel");
+    }
+
+    public void processQuery(String query, WebSocketSession session, String modelName) {
+        OpenAiStreamingChatModel model = models.getOrDefault(modelName, models.get("gpt4oMiniStreamingModel"));
+
+        List<ChatMessage> messages = List.of(
+                new SystemMessage("You are a helpful AI agent, you answer short and concise."),
+                new SystemMessage("You are an expert in Java and Angular."),
+                new UserMessage(query)
+        );
 
         MyStreamingResponseHandler responseHandler = new MyStreamingResponseHandler(session);
         model.chat(ChatRequest.builder().messages(messages).build(), responseHandler);
